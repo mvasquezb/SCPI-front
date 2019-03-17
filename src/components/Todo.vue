77856082863d2d37954cbc75d0455bd713846aee
Martín Vásquez <pedromvasquezb@gmail.com>
2019-03-17 16:43:43 -0500

    Add Todo component

---
+++ src/components/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="header">
+        <h1>todos</h1>
+        <input class="new-todo" autofocus autocomplete="off"
+                placeholder="What needs to be done ?"
+                v-model="newTodo"
+                @keyup.enter="addTodo" />
+    </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Todo extends Vue {
+    newTodo = ''
+
+    addTodo() {
+        let value = this.newTodo && this.newTodo.trim();
+        if (!value) {
+            return;
+        }
+        this.$emit('add-todo', value);
+        this.newTodo = '';
+    }
+}
+</script>
