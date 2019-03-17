30bac71fb0a28e31fe7224af5b1f5de6f0ebfaff
Martín Vásquez <pedromvasquezb@gmail.com>
2019-03-17 16:44:02 -0500

    View wrap up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home todoapp">
+    <Todo v-on:add-todo="addTodo" />
+    <TodoList v-bind:todos="todos" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Todo from '@/components/Todo.vue';
+import TodoList from '@/components/TodoList.vue';
+import { Todo as TodoModel } from '@/models.ts';
 
 @Component({
   components: {
-    HelloWorld,
+    Todo,
+    TodoList
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  todos: Array<TodoModel> = []
+
+  addTodo(newTodo: string) {
+    this.todos.push(new TodoModel(newTodo));
+  }
+}
 </script>
