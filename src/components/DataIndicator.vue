6f72e96d65127eb5d26733bfa87fb70b31b810b2
Martín Vásquez <pedromvasquezb@gmail.com>
2019-05-01 19:03:50 -0500

    Add DataIndicator component

---
+++ src/components/DataIndicator.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="indicator-container">
+    <p class="title">{{ title }}</p>
+    <p class="value">{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    value: {
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.indicator-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+}
+
+.title {
+  margin-bottom: 0.5rem;
+  font-weight: bold;
+}
+
+.value {
+  align-self: center;
+  background-color: white;
+  padding: 10px 25px;
+  margin-left: 10px;
+  margin-right: 10px;
+  font-size: 1.5em;
+  border: 3px solid grey;
+}
+</style>
