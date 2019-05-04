cf7d3a196cd0bc09cede7d86792c096357a8dacd
Martín Vásquez <pedromvasquezb@gmail.com>
2019-05-04 16:03:50 -0500

    Add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import router from './router'
 import store from './store'
 import http from './http';
 import PaperDashboard from "./plugins/paperDashboard";
+import BootstrapVue from 'bootstrap-vue'
 import "vue-notifyjs/themes/default.css";
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
 Vue.use(PaperDashboard);
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
