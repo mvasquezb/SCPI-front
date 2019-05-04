31565dd0d24945caf897c35b7824f1ef6429f9f7
Martín Vásquez <pedromvasquezb@gmail.com>
2019-05-04 15:35:32 -0500

    Add WagonList, Oven components

---
+++ src/components/Oven.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="bg-white oven">
+    <div class="row">
+      <div class="col-3">
+        <h4>{{ oven.code }}</h4>
+      </div>
+      <div class="col-3 offset-6 text-right">
+        <h4>*Prod</h4>
+      </div>
+    </div>
+    <div class="row">
+      <WagonList class="col-9 wagons" :wagons="oven.wagons" />
+      <div class="col-3 wagon-indicators">
+        <DataIndicator title="Vagoneta Inicial" :value="44" />
+        <DataIndicator title="Vagoneta Actual" :value="48" />
+      </div>
+    </div>
+    <div class="row d-flex oven-actions">
+      <div class="col-5 text-center">
+        <button class="btn btn-main">Aceptar</button>
+      </div>
+      <div class="col-2"></div>
+      <div class="col-5 text-center">
+        <button class="btn btn-main">Libre</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import WagonList from '@/components/WagonList.vue';
+import DataIndicator from '@/components/DataIndicator.vue';
+
+export default {
+  props: {
+    oven: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {
+    WagonList,
+    DataIndicator
+  }
+};
+</script>
+
+<style lang="scss">
+.wagons {
+  height: 300px;
+  overflow-y: auto;
+  margin-bottom: 15px;
+}
+
+.wagon-indicators {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+
+.oven {
+  padding: 10px;
+}
+
+.oven-actions {
+  button {
+    padding-top: 10px;
+    padding-bottom: 10px;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/WagonList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wagon-container">
+    <p v-for="wagon in wagons" class="wagon bg-nude" :key="wagon.id">
+      {{ wagon.code }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    wagons: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.wagon {
+  text-align: center;
+  padding: 20px;
+  font-weight: bold;
+  font-size: 1.1rem;
+}
+</style>
