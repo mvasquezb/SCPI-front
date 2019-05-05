d56cb077e503e59637fdeed64ef46e871c6df2cf
Martín Vásquez <pedromvasquezb@gmail.com>
2019-05-04 20:50:21 -0500

    Remove unused sidebar links

--- src/layout/dashboard/DashboardLayout.vue
+++ src/layout/dashboard/DashboardLayout.vue
@@ -3,12 +3,12 @@
     <side-bar backgroundColor="darkblue" activeColor="primary">
       <template slot="links">
         <sidebar-link to="/" name="Inicio" icon="ti-panel"/>
-        <sidebar-link to="/about" name="About" icon="ti-user"/>
+        <!-- <sidebar-link to="/about" name="About" icon="ti-user"/>
         <sidebar-link to="/table-list" name="Table List" icon="ti-view-list-alt"/>
         <sidebar-link to="/typography" name="Typography" icon="ti-text"/>
         <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2"/>
         <sidebar-link to="/maps" name="Map" icon="ti-map"/>
-        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/>
+        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/> -->
       </template>
       <mobile-menu>
         <li class="nav-item">
