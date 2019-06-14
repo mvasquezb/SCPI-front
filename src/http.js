be0ae91dd7922451398375dfeaf833ad5b76bb23
Martín Vásquez <pedromvasquezb@gmail.com>
2019-06-14 17:50:30 -0500

    Update .env

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? process.env.API_URL : 'http://localhost:7000',
+    baseURL: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_URL : 'http://localhost:7000',
     timeout: 30000,
 });
 
