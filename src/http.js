21d0825f33bb28b2c658460a7fe6e05ef29c5421
Martín Vásquez <pedromvasquezb@gmail.com>
2019-06-29 09:41:23 -0500

    Remove request timeout

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const http = axios.create({
     baseURL: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_URL : 'http://localhost:7000',
-    timeout: 60000,
+    // timeout: 60000,
 });
 
 export default http;
